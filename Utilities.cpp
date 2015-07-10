a2f3e159f51ac332b05ef8f27a51db11e38f89c5
xjlt <x.jason.turner@gmail.com>
2015-07-10 15:02:18 -0300

    Added Utilties.cpp description

--- Utilities.cpp
+++ Utilities.cpp
@@ -51,71 +51,71 @@ namespace Utilities {
 
 		Utilities* util = NULL;
 
-		while(runUtil != 0){
-			cout << "::Utilities Menu::" << endl;
-			cout << "1. Converter Utility" << endl;
-			cout << "2. Encryption Utility" << endl;
-			cout << "3. Message of the day Utility" << endl;
-			cout << "4. Palindrome Utility" << endl;
-			cout << "5. Calculator Utility" << endl;
-			cout << "6. Display system date/time" << endl;
-			cout << "0. Quit" << endl;
-			cout << endl;
-			cout << "Enter Utility to run (1-1 or 0 to quit): ";
+		//while(runUtil != 0){
+		cout << "::Utilities Menu::" << endl;
+		cout << "1. Converter Utility" << endl;
+		cout << "2. Encryption Utility" << endl;
+		cout << "3. Message of the day Utility" << endl;
+		cout << "4. Palindrome Utility" << endl;
+		cout << "5. Calculator Utility" << endl;
+		cout << "6. Display system date/time" << endl;
+		cout << "0. Quit" << endl;
+		cout << endl;
+		cout << "Enter Utility to run (1-1 or 0 to quit): ";
 
-			cin >> runUtil;
+		cin >> runUtil;
 
-			if(!cin){
-				cout << "Error - Please enter a valid menu selection" << endl;
-				runUtil = -1;
+		if(!cin){
+			cout << "Error - Please enter a valid menu selection" << endl;
+			runUtil = -1;
 
-				cin.clear();
-				cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			}
-			else{
-
-				switch(runUtil){
-					case CONVERTER_UTILITY:
-
-						cout << "converter" << endl;
-
-						util = new Converter();
-
-						break;
-					case ENCRYPTION_UTILITY:
-
-						cout << "encryption" << endl;
-						break;
-					case MOTD_UTILITY:
-
-						cout << "motd" << endl;
-						break;
-					case PALINDROME_UTILITY:
-
-						cout << "palindrome" << endl;
-						break;
-					case CALCULATOR_UTILITY:
-
-						cout << "calc" << endl;
-						break;
-					case DATE_TIME_UTILITY:
-
-						cout << "date time" << endl;
-						break;
-
-					case EXIT:
-
-						cout << "Exiting Utilites!" << endl;
-						break;
-					default:
-						cout << "Error - Please enter a valid menu selection" << endl;
-						runUtil = -1;
-						break;
-				}
-
-				return util;
-			}
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		}
+		else{
+
+			switch(runUtil){
+				case CONVERTER_UTILITY:
+
+					cout << "converter" << endl;
+
+					util = new Converter();
+
+					break;
+				case ENCRYPTION_UTILITY:
+
+					cout << "encryption" << endl;
+					break;
+				case MOTD_UTILITY:
+
+					cout << "motd" << endl;
+					break;
+				case PALINDROME_UTILITY:
+
+					cout << "palindrome" << endl;
+					break;
+				case CALCULATOR_UTILITY:
+
+					cout << "calc" << endl;
+					break;
+				case DATE_TIME_UTILITY:
+
+					cout << "date time" << endl;
+					break;
+
+				case EXIT:
+
+					cout << "Exiting Utilites!" << endl;
+					break;
+				default:
+					cout << "Error - Please enter a valid menu selection" << endl;
+					runUtil = -1;
+					break;
+			}
+
+			return util;
+		}
+		//}
 
 	}
 
