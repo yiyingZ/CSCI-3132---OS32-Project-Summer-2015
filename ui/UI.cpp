519e896b875b6d7fb1aeeb78867f65715b1672ff
Nathan Schucher <nathan.schucher@gmail.com>
2015-07-18 14:10:52 -0300

    Added more required functionality to user interface class (readline, commandValidate).

--- ui/UI.cpp
+++ ui/UI.cpp
@@ -2,19 +2,53 @@
 // Created by nathan on 17/07/15.
 //
 
+#include <iterator>
+#include <sstream>
+#include <algorithm>
 #include "UI.h"
 
-/*
- * Prints a line to stdout with a new line after
- */
 void UI::println(const std::string &msg) {
     std::cout << msg << std::endl;
 }
 
-/*
- * Prints a line to stdout without a new line after
- */
 void UI::print(const std::string &msg) {
     std::cout << msg;
 }
 
+void UI::startUI() {
+    // Authenticate User:
+
+    // Start prompt
+    UI::println("Your main menu options");
+    int option = 0;
+    while( option = UI::read<int>()) {
+        if(option == 1) { // Parse user command
+            std::string line = UI::readLine();
+            auto cmd = parseCommand(line);
+            if(commandValidate(cmd)) {
+                // scheduler->invokeCommand(cmd)
+            }
+        }
+        else if(option == 2) { // Start utilties
+
+        }
+        else if(option == 3) { // Shutdown
+            break;
+        }
+        else {
+            UI::println("Invalid option.");
+        }
+    }
+}
+
+std::vector<std::string> UI::parseCommand(const std::string &line) {
+    std::istringstream iss;
+    std::vector<std::string> command_tokens;
+    std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::back_inserter(command_tokens));
+
+    return command_tokens;
+}
+
+bool UI::commandValidate(std::vector<std::string> &cmd) {
+    return false;
+}
--- ui/UI.h
+++ ui/UI.h
@@ -5,18 +5,54 @@
 #ifndef UI_UI_H
 #define UI_UI_H
 
+#include <vector>
 #include <iostream>
 
 class UI {
+private:
+    /*
+     * Validates that this command is a valid system command.
+     */
+    bool commandValidate(std::vector<std::string> &cmd);
+
+    /*
+     * Splits the command entered by the user into a vector of strings
+     */
+    std::vector<std::string> parseCommand(const std::string &line);
 public:
+
+    /*
+     * Starts the shell / command parsing loop.
+     */
+    void startUI();
+
+    /*
+     * Prints a line to stdout with a new line after
+     */
     static void println(const std::string &);
+
+    /*
+     * Prints a line to stdout without a new line after
+     */
     static void print(const std::string &);
 
+    /*
+     * Reads the parametrised type from stdin and returns it.
+     */
     template <typename T>
     static T read() {
         T ret;
         std::cin >> ret;
         return ret;
+    }
+
+    /*
+     * Reads the parametrised type from stdin and returns it.
+     */
+    static std::string readLine() {
+        std::string line;
+        std::getline(std::cin, line);
+        return line;
     }
 };
 
