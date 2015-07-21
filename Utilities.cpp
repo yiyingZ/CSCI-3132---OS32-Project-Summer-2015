fdce6a4c8c1a93e9904ec51edbfeb58fdf52ade8
Jason Turner <jturner@linux-04nn.site>
2015-07-21 20:12:17 -0300

    updated Utilities to loop until 0 is entered and Converter with conversion formulas

--- Utilities.cpp
+++ Utilities.cpp
@@ -51,69 +51,67 @@ namespace Utilities {
 
 		Utilities* util = NULL;
 
-		while(runUtil == -1 && runUtil != 0){
-			cout << "::Utilities Menu::" << endl;
-			cout << "1. Converter Utility" << endl;
-			cout << "2. Encryption Utility" << endl;
-			cout << "3. Message of the day Utility" << endl;
-			cout << "4. Palindrome Utility" << endl;
-			cout << "5. Calculator Utility" << endl;
-			cout << "6. Display system date/time" << endl;
-			cout << "0. Quit" << endl;
-			cout << "\nEnter Utility to run (1-6, 0 to Quit): ";
+		cout << "::Utilities Menu::" << endl;
+		cout << "1. Converter Utility" << endl;
+		cout << "2. Encryption Utility" << endl;
+		cout << "3. Message of the day Utility" << endl;
+		cout << "4. Palindrome Utility" << endl;
+		cout << "5. Calculator Utility" << endl;
+		cout << "6. Display system date/time" << endl;
+		cout << "0. Quit" << endl;
+		cout << "\nEnter Utility to run (1-6, 0 to Quit): ";
 
-			cin >> runUtil;
+		cin >> runUtil;
 
-			if(!cin){
-				cout << "Error - Please enter a valid menu selection" << endl;
-				runUtil = -1;
+		if(!cin){
+			cout << "Error - Please enter a valid menu selection" << endl;
+			runUtil = -1;
 
-				cin.clear();
-				cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else{
+
+			switch(runUtil){
+				case CONVERTER_UTILITY:
+
+					cout << "converter" << endl;
+
+					util = new Converter();
+
+					break;
+				case ENCRYPTION_UTILITY:
+
+					cout << "encryption" << endl;
+					break;
+				case MOTD_UTILITY:
+
+					cout << "motd" << endl;
+					break;
+				case PALINDROME_UTILITY:
+
+					cout << "palindrome" << endl;
+					break;
+				case CALCULATOR_UTILITY:
+
+					cout << "calc" << endl;
+					break;
+				case DATE_TIME_UTILITY:
+
+					cout << "date time" << endl;
+					break;
+
+				case EXIT:
+					util = NULL;
+					cout << "Exiting Utilites!" << endl;
+					break;
+				default:
+					cout << "Error - Please enter a valid menu selection" << endl;
+					runUtil = -1;
+					break;
 			}
-			else{
 
-				switch(runUtil){
-					case CONVERTER_UTILITY:
-
-						cout << "converter" << endl;
-
-						util = new Converter();
-
-						break;
-					case ENCRYPTION_UTILITY:
-
-						cout << "encryption" << endl;
-						break;
-					case MOTD_UTILITY:
-
-						cout << "motd" << endl;
-						break;
-					case PALINDROME_UTILITY:
-
-						cout << "palindrome" << endl;
-						break;
-					case CALCULATOR_UTILITY:
-
-						cout << "calc" << endl;
-						break;
-					case DATE_TIME_UTILITY:
-
-						cout << "date time" << endl;
-						break;
-
-					case EXIT:
-
-						cout << "Exiting Utilites!" << endl;
-						break;
-					default:
-						cout << "Error - Please enter a valid menu selection" << endl;
-						runUtil = -1;
-						break;
-				}
-
-				return util;
-			}
+			return util;
 		}
 
 	}
@@ -124,12 +122,17 @@ namespace Utilities {
 	 *
 	 */
 	 void Utilities::execute(){
-		 Utilities* util = displayMenu();
+		 Utilities* util;
 
-		 //This will be replaced with reference to the Kernel and the pointer "util" passed for scheduling and execution
-		 if(util != NULL){
+		 while(util != NULL){
+			 util = displayMenu();
 			 util->execute();
 		 }
+
+		 //This will be replaced with reference to the Kernel and the pointer "util" passed for scheduling and execution
+		 /*if(util != NULL){
+			 util->execute();
+		 }*/
 
 
 		 //TODO need access to the memory management system in order to pass the pointer to the created utility object
