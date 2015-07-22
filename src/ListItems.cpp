959c90334ffc0bd3f5be9110421019fb06be53ba
Arnold Zoundi <=>
2015-07-22 15:32:45 -0300

    Added comments
    Added comments

--- src/ListItems.cpp
+++ src/ListItems.cpp
@@ -1,22 +1,11 @@
 /*
- * test.cpp
- *
- *  Created on: Jul 21, 2015
- *      Author: ArnoldBocor
- */
-
-/*
  * ListItems.cpp
  *
- * This class is use to print the list the of files and directories in the path provided
- * by the user. It takes the path and print the names of the current files and directories
- * in it.
+ * This class is use to print the list the of files and directories in the current directory.
+ * It takes the path and print the names of the current files and directories in it.
  *
  * This class is very dependent on the file structure implemented by the Kernel Group 1.
  * I might make some changes depending on what this group implemented.
- *
- * This class will also be altered to respect the design guidelines by extending the command
- * class.
  *
  * The solution designed for this command works with both Windows and Linux file structure.
  *
@@ -36,28 +25,40 @@ class ListItems:public Command{
 
 public:
 
-	//constructor
+	/*
+	 * Constructor
+	 */
 	ListItems(){
 
 	}
 
 	/**
-	 * Iterate through the files and directories and print their names to stdout
+	 * Iterate through the files and directories in the current directory
+	 *  and print their names to stdout
 	 */
 	int execute(){
 
+		//create pointer to directory
 		DIR *dir;
 		string oneDot=".";
 		string twoDot="..";
+
+		//Initialize dirent structure
 		struct dirent *ent;
+
+		//Open current directory, iterate through it and display the files and folders names.
 		if((dir = opendir(".")) != NULL){
 
+			//Iterate until there is no more item left in the current directory
 			while((ent = readdir(dir)) != NULL){
 
+				//Do not display dotted names
 				if(ent->d_name != oneDot && ent->d_name != twoDot)
 				cout<<ent->d_name<<"\n";
+
 			}
 
+			//Close the current directory
 			closedir(dir);
 
 			return 1;
